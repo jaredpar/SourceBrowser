ced9a12f1a4b4fcb7f7cbb101367776c22ce3db4
Jared Parsons <jared@paranoidcoding.org>
2023-12-21 10:34:03 -0800

    Cleaned up the display a bit

--- src/SourceIndexServer/Pages/Upload.cshtml.cs
+++ src/SourceIndexServer/Pages/Upload.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Build.Evaluation;
@@ -9,8 +10,10 @@ public class UploadModel : PageModel
     private RepositoryManager Manager { get; }
     private RepositoryGenerator Generator { get; }
 
+    public string ErrorMessage { get; set; }
+
     [BindProperty]
-    public string ProjectName { get; set; }
+    public string RepositoryName { get; set; }
 
     [BindProperty]
     public IFormFile Upload { get; set; }
@@ -23,6 +26,18 @@ public class UploadModel : PageModel
 
     public async Task<IActionResult> OnPostAsync()
     {
+        if (string.IsNullOrEmpty(RepositoryName))
+        {
+            ErrorMessage = "Repository name cannot be empty";
+            return Page();
+        }
+
+        if (Upload is null || Path.GetExtension(Upload.FileName) != ".complog")
+        {
+            ErrorMessage = "Must provide a compiler log file";
+            return Page();
+        }
+
         using var stream = Upload.OpenReadStream();
         var dirName = await Generator.Generate(stream);
         Manager.AddRepository(dirName);
